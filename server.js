781367a78d93267552c81d35046af3d6d9c8f8e2
Svyatoslav Suprun <72302517+GOFT-lab@users.noreply.github.com>
2025-01-27 18:15:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-require('dotenv').config(); // Підключення dotenv
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const moobotRoutes = require('./routes/moobotRoutes');
 
 const app = express();
 
-// Отримуємо змінні середовища з .env або через Vercel Environment Variables
 const PORT = process.env.PORT || 3000;
 const ROOT_LINK = process.env.ROOT_LINK || 'http://localhost';
 
-// Middleware для обробки JSON
 app.use(bodyParser.json());
 
-// Підключаємо роут для Moobot
 app.use('/moobot', moobotRoutes);
 
-// Для Vercel необхідно використовувати експорт сервера замість прослуховування порту
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Сервер працює на ${ROOT_LINK}:${PORT}`);
+});
