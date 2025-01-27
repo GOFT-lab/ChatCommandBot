5631de843b97458d0cc5c1d7deb4eb6f332fc92c
Svyatoslav Suprun <72302517+GOFT-lab@users.noreply.github.com>
2025-01-27 18:20:06 +0200

    Update moobotRoutes.js

--- routes/moobotRoutes.js
+++ routes/moobotRoutes.js
@@ -3,7 +3,7 @@ const ageHandler = require('../commands/ageCommand');
 
 const router = express.Router();
 
-router.get('/moobot/age', ageHandler);
+router.get('/age', ageHandler);
 
 router.get('/', (req, res) => {
   res.json({ message: 'Сервер запущено!' });
