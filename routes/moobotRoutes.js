49efa9a660cd097d3f463e159fe869da36b3c59c
Svyatoslav Suprun <72302517+GOFT-lab@users.noreply.github.com>
2025-02-21 21:35:58 +0200

    Update moobotRoutes.js

--- routes/moobotRoutes.js
+++ routes/moobotRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const ageHandler = require('../commands/ageCommand');
 const ball8Handler = require('../commands/ball8Command');
+const selfBanHandler = require('../commands/selfBanCommand');
 
 const router = express.Router();
 
 router.get('/age', ageHandler);
 router.get('/ball8', ball8Handler);
+router.get('/ban', selfBanHandler);
 
 router.get('/', (req, res) => {
   res.json({ message: 'Сервер запущено!' });
